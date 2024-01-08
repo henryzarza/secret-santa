507bfdd4bd5a8597d31ae0be695419188ddffcc3
Henry Zarza <henryacz7@gmail.com>
2024-01-08 17:08:29 -0500

    CSS 1 - Icons

---
+++ web/src/components/Icon/Icon.tsx
@@ -0,0 +1,9 @@
+const Icon = ({ size = 24, id }: { size: number; id: string }) => {
+  return (
+    <svg width={size} height={size}>
+      <use href={`/icons/sprite.svg#${id}`} />
+    </svg>
+  )
+}
+
+export default Icon
