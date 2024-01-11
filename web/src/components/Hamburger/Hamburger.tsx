885941747990f46408d0e10d5dc2cdec9f7f4ca4
Henry Zarza <henryacz7@gmail.com>
2024-01-11 10:57:12 -0500

    CSS 7 - Hamburger menu

---
+++ web/src/components/Hamburger/Hamburger.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+const Hamburger = () => {
+  const [isHamburger, setIsHamburger] = useState(true)
+
+  return (
+    <button
+      className="flex h-[58px] w-[68px] flex-col items-center justify-center bg-supernova"
+      onClick={() => setIsHamburger((prevValue) => !prevValue)}
+    >
+      <div
+        className={`absolute h-[3px] w-12 bg-black transition-transform ${
+          isHamburger ? '-translate-y-3' : 'rotate-45'
+        }`}
+      />
+      <div className={`h-[3px] w-12 bg-black ${isHamburger ? '' : 'hidden'}`} />
+      <div
+        className={`absolute h-[3px] w-12 bg-black transition-transform ${
+          isHamburger ? 'translate-y-3' : '-rotate-45'
+        }`}
+      />
+    </button>
+  )
+}
+
+export default Hamburger
