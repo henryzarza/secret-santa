4fc38813f24ed87a1925c0621a93c22d62a4167c
Henry Zarza <henryacz7@gmail.com>
2024-01-25 19:33:03 -0500

    Wish list page

--- web/src/components/WishListField/index.tsx
+++ web/src/components/WishListField/index.tsx
@@ -9,37 +9,37 @@ interface WishListFieldProps {
 
 const WishListField = ({ name, number, roundButton }: WishListFieldProps) => {
   return (
-    <div className="grid grid-cols-2 items-center gap-x-5 gap-y-2">
-      <div className="flex items-center bg-white">
-        <div>
-          <label htmlFor={name} className="pl-8 pr-5 text-3xl">
-            {number}
-          </label>
-        </div>
-        <input
-          type="text"
-          id={name}
-          name={name}
-          className="font-bold outline-none"
-        />
-      </div>
-      <div className={roundButton.status === 'error' ? '' : ''}>
-        <RoundButton {...roundButton} />
-      </div>
-      <div className="relative flex items-start gap-2 pl-8">
-        <img src="/images/l-bracket.svg" alt="bracket" />
-        <div className="flex flex-1 items-center bg-white">
-          <div className="px-6">
-            <label htmlFor={`${name}-url`}>URL</label>
+    <div className="flex items-start gap-5">
+      <div className="flex flex-col gap-x-5 gap-y-2 w-full">
+        <div className="flex items-center bg-white">
+          <div>
+            <label htmlFor={name} className="pl-8 pr-5 text-3xl">
+              {number}
+            </label>
           </div>
           <input
-            type="url"
-            name={`${name}-url`}
-            id="url"
-            className="short flex-1"
+            type="text"
+            id={name}
+            name={name}
+            className="font-bold outline-none"
           />
         </div>
+        <div className="flex items-start gap-2 md:pl-8">
+          <img src="/images/l-bracket.svg" alt="bracket" />
+          <div className="flex flex-1 items-center bg-white">
+            <div className="px-6">
+              <label htmlFor={`${name}-url`}>URL</label>
+            </div>
+            <input
+              type="url"
+              name={`${name}-url`}
+              id="url"
+              className="short flex-1"
+            />
+          </div>
+        </div>
       </div>
+      <RoundButton {...roundButton} />
     </div>
   )
 }
---
+++ web/src/pages/WishListPage/WishListPage.tsx
@@ -0,0 +1,25 @@
+import { MetaTags } from '@redwoodjs/web'
+import WishListField from 'src/components/WishListField'
+
+const WishListPage = () => {
+  return (
+    <>
+      <MetaTags title="WishList" description="WishList page" />
+
+      <h1 className="font-condensed text-7xl leading-[0.8] lg:text-[116px] uppercase text-white font-normal mb-2">
+        Wish list
+      </h1>
+      <h2 className="font-handwriting font-bold text-xl lg:text-[32px] text-white uppercase mb-9">
+        Make your list and check it twice
+      </h2>
+      <div className="flex flex-col gap-6 pb-10">
+        <WishListField number={1} name="wishList-1" roundButton={{ status: "error" }} />
+        <WishListField number={2} name="wishList-2" roundButton={{ status: "error" }} />
+        <WishListField number={3} name="wishList-3" roundButton={{ status: "success" }} />
+        <button type="submit">Submit</button>
+      </div>
+    </>
+  )
+}
+
+export default WishListPage
